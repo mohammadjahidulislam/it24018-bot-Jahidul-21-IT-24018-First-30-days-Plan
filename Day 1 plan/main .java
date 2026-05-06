d705a44cf364efb24b7b9fcd7b080ab7ce3ed10c
mohammadjahidulislam <mohammadjahidulislam18@gmail.com>
2026-05-06 11:47:06 +0600

    Add files via upload

---
+++ Day 1 plan/main .java
@@ -0,0 +1,11 @@
+public class main{
+    static void main(String[] args){
+        Student s1=new Student();
+        s1.name="Md Jahidul Islam";
+        s1.Roll=IT24018;
+        s1.district="Tangail";
+        s1.Number="01775087826";
+        s1.Show();
+       
+    }
+}
